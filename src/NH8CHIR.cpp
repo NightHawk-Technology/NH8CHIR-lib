9c0444e2c3b06a71033838fda202f963acf2c176
Chawanwit Akarajirathanachot <76256502+Karibura-Cyber@users.noreply.github.com>
2024-04-19 20:49:44 +0700

    Update NH8CHIR.cpp

--- src/NH8CHIR.cpp
+++ src/NH8CHIR.cpp
@@ -2,13 +2,11 @@
 #include "NH8CHIR.h"
 #include "Wire.h"
 
-bool ads_vref_int_enabled = 0;  // default voltage reference is external
-
-// command address for the channels, allows 0-7 channel mapping in the correct order
+bool ads_vref_int_enabled = 0;
 unsigned char channels[8] = {0x00, 0x40, 0x10, 0x50, 0x20, 0x60, 0x30, 0x70}; 
 
 NH8CHIR::NH8CHIR(unsigned char _address){
-  ads_address = _address;   // Set ADC i2c address to the one passed to the function
+  ads_address = _address;
 }
 
 void NH8CHIR::init(){
@@ -17,34 +15,34 @@ void NH8CHIR::init(){
 
 void NH8CHIR::init(boolean _vref){
   Wire.begin();   // Initialize I2C Bus
-  ads_vref_int_enabled = _vref;  // Set vref trigger to specified value (Internal or external)
+  ads_vref_int_enabled = _vref;
 }
 
 unsigned int NH8CHIR::read(unsigned char channel, bool mode)
 {
-  unsigned char command = 0;		// Initialize command variable
-  unsigned int reading = 0;			// Initialize reading variable
+  unsigned char command = 0;
+  unsigned int reading = 0;	
   
-  command = channels[channel];      // put required channel address in command variable
+  command = channels[channel];
   
   if (mode){
-    command = command ^ 0x80; 		// Enable Single-ended mode (toggle MSB, SD bit in datasheet)
+    command = command ^ 0x80; 
   }
   if (ads_vref_int_enabled){ 
-    command = command ^ 0x08; 	    // Enable internal voltage reference if ads_vref_int_enabled = 1
+    command = command ^ 0x08; 
   }
 
-  Wire.beginTransmission(ads_address); 	// Send a start or repeated start command with a slave address and the R/W bit set to '0' for writing.
-  Wire.write(command);      			// Then send a command byte for the register to be read.
-  Wire.endTransmission();				// Send stop command
+  Wire.beginTransmission(ads_address);
+  Wire.write(command); 
+  Wire.endTransmission();	
   
-  Wire.requestFrom(ads_address, 2);		// Request 2 bytes from the ADC
+  Wire.requestFrom(ads_address, 2);
 
-  if(2 <= Wire.available())    		// if two bytes were received
+  if(2 <= Wire.available()) 
   {
-    reading = Wire.read();     		// receive high byte
-    reading = reading << 8;    		// shift high byte to be high 8 bits
-    reading |= Wire.read();    		// receive low byte into lower 8 bits
+    reading = Wire.read();
+    reading = reading << 8; 
+    reading |= Wire.read(); 
   }
-  return reading;					// return the full 12 bits reading from the ADC channel
+  return reading;		
 }
